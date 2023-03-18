924878e3042f3687702400344925b71f8835d2c9
blessing <budeji@gmail.com>
2023-03-17 18:57:01 -0700

    my commit for 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+
+	int n;
+	int a;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (a = 97; a <= 102; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
