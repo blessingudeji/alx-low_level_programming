79126179bdeaeb990e0f005100585eedc5205705
blessing <budeji@gmail.com>
2023-03-22 10:35:46 -0700

    My commit for 0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return:always return 0
+ *
+ */
+
+void positive_or_negative(int i)
+{
+
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}
