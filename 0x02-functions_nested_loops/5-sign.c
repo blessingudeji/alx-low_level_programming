15fca274542506357004b3062a8744835ade670a
blessing <budeji@gmail.com>
2023-03-21 17:46:47 -0700

    My second commit after fixing some errors on line 15

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - main function
  *
- * @n: input number as an integer
+ * @n: character to compare
  *
  * Return: 1 if positive, 0 if zero, -1 if negative
  *
@@ -12,20 +12,14 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	if (n == 0)
 	{
-		_putchar(45);
+		_putchar('0');
 		return (0);
 	}
-	else
-	{
-	_putchar(48);
-	return (0);
-	}
-	_putchar('\n');
-
-
+	_putchar('-');
+	return (-1);
 }
