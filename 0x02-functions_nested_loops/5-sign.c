bd4b5b75980ceda3d04c76eb22cb952f134b1676
blessing <budeji@gmail.com>
2023-03-21 17:37:12 -0700

    My first commit for 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_sign - main function
+ *
+ * @n: input number as an integer
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ *
+ */
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (0);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+	_putchar('\n');
+
+
+}
