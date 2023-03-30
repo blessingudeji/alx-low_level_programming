1e9f0cb86a56da395128c77b10f9eadab24a0626
Blessing Udeji <61279751+blessingudeji@users.noreply.github.com>
2023-03-30 18:43:29 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,6 @@
  * string_toupper - this function changes the content of a string to uppercase
  * @str: string to change
  * Return: a pointer to uppercase str
- * Date: 01-03-2023
  */
 char *string_toupper(char *str)
 {
