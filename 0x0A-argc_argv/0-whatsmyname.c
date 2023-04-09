95f84f800e56f7f004e0a08d77c5d032d5bf6565
blessing <budeji@gmail.com>
2023-04-09 15:21:16 -0700

    My commit for 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main function
+* @argc: argument counter
+* @argv: argument vector
+* Return: 0
+*/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
