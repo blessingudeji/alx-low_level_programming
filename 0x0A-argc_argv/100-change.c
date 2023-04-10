53f9e2bd66d9d7571d780cce1a9dfafdc6cac88a
blessing <budeji@gmail.com>
2023-04-09 19:20:10 -0700

    My commit for 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main function
+* @argc: argument counter
+* @argv: argument vector
+* Return: 0 for success otherwise 1
+*/
+int main(int argc, char **argv)
+{
+	int cents, coins = 0;
+
+	/* checking the number of arguments*/
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	/* counting the number of the coins*/
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else
+			cents -= 1;
+		coins++;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
+
