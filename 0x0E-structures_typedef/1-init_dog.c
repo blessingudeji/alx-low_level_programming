d3a996f6b07bc5e95d07e6934b1476268206d7e5
blessing <budeji@gmail.com>
2023-04-17 17:41:01 -0700

    my commit for 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+* init_dog - a function that initialize a variable of type
+* @d: struct dog
+* @name: the name of the dog
+* @age: the dog's age
+* @owner: the owner of the dog
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
+
