ec14dccbeecddef9dca7d41ee414c91d50bd6932
Blessing <budeji@gmail.com>
2023-05-08 20:01:31 -0700

    My commit for 0-read_textfile.c task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * read_textfile - This function reads a textfile
+ * @filename: The file to be read
+ * @letters: amount of bytes to read
+ * Return: actual number of letters that could be read
+ * or prntd
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t prntd, wrten;
+	int fd;
+	char *buff;
+
+	/*Allocate memory for the buff*/
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
+
+	/*Check if the filename is NULL*/
+	if (filename == NULL)
+		return (0);
+
+	/*Open file in read-write mode*/
+	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDONLY);
+	wrten = read(fd, buff, letters);
+	prntd = write(STDOUT_FILENO, buff, wrten);
+	if (fd == -1 || wrten == -1 || prntd == -1 || prntd != wrten)
+	{
+		free(buff);
+		return (0);
+	}
+	/* Always close the file descriptor*/
+	close(fd);
+
+	/*Free the memory allocated to the buffer*/
+	free(buff);
+
+	/* Return the number of letters read or printed*/
+	return (prntd);
+}
